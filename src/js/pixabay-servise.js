bc135e714e7abc28e1d6a8a983004f824a70351c
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-26 23:59:29 +0100

    Update Pixabay servise

---
+++ src/js/pixabay-servise.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const API_KEY = '22469389-b601f8ee0455705c879309776';
+const BASE_URL = 'https://pixabay.com/api';
+axios.defaults.baseURL = BASE_URL;
+
+class Gallery {
+  constructor() {
+    this.key = API_KEY;
+    this.requestQuery = '';
+    this.imageType = 'photo';
+    this.orientation = 'horizontal';
+    this.filter = true;
+    this.page = '1';
+    this.quantity = '40';
+  }
+
+  async getGalerry() {
+    const response = await axios.get(
+      `/?key=${API_KEY}&image_type=${this.imageType}&page=${this.page}&per_page=${this.quantity}&orientation=${this.orientation}&q=${this.requestQuery}&safesearch=${this.filter}`,
+    );
+    const { hits, totalHits } = await response.data;
+    // console.log(imgData);
+    return { hits, totalHits };
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.requestQuery;
+  }
+
+  set query(newQuery) {
+    this.requestQuery = newQuery;
+  }
+}
+
+// const gallery = new Gallery();
+
+// gallery.getGalerry();
